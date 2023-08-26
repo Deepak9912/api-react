6eb57793c3b309fa785708da6e02db33e35b0e89
Deepak9912 <DEEPAKSINGHJ72@GMAIL.COM>
2023-08-26 19:49:26 +0000

    deployment

--- task_api/src/App.js
+++ task_api/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import MovieList from './component/MovieList';
+import AddMovie from './component/AddMovie';
 
 function App() {
 
@@ -17,28 +18,44 @@ function App() {
     setError(null);
 
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch('https://practice-21ee7-default-rtdb.firebaseio.com/movies.json');
       // error state to catch any error while fetching data
       if(!response.ok){
         throw new Error('Something went wrong');
       }
       const data = await response.json();
 
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          opening_crawl: movieData.opening_crawl,
-          producer: movieData.producer,
-          release_date: movieData.release_date
-        }
-      })
-      setMovies(transformedMovies); 
+      const loadedMovies = [];
+
+      for (const key in data){
+        loadedMovies.push({
+          id:key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        })
+      }
+
+      setMovies(loadedMovies); 
     } catch (error){
       // we catch the error here
       setError(error.message);
     }
     setIsLoading(false); 
+  }
+
+  async function addMovieHandler(movie){
+    const response = await fetch('https://practice-21ee7-default-rtdb.firebaseio.com/movies.json', {
+      method: 'POST',
+      // body doesn't take js object, it takesJSON format data
+      body: JSON.stringify(movie),
+      // header is not required by firebase. header describe to contect that will be sent
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await response.json();
+    console.log(data);
   }
 
   let content = <p>Found no movies!</p>
@@ -58,6 +75,7 @@ function App() {
   return (
     <div>
       <section>
+        <AddMovie onAddMovie={addMovieHandler} />
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
       {content}
---
+++ task_api/src/component/AddMovie.js
@@ -0,0 +1,40 @@
+import { useRef } from "react";
+
+// useRef is used to store multiple values
+
+const AddMovie = (props) => {
+  const titleRef = useRef('');
+  const openingTextRef = useRef('');
+  const releaseDateRef = useRef('');
+
+  function submitHandler(event){
+    event.preventDefault();
+
+    const movie = {
+      title: titleRef.current.value,
+      openingText: openingTextRef.current.value,
+      releaseDate: releaseDateRef.current.value,
+    };
+
+    props.onAddMovie(movie);
+  }
+
+
+  return <form onSubmit={submitHandler}>
+    <div>
+      <label htmlFor="title">Title</label>
+      <input type="text" id="title" ref={titleRef} />
+    </div>
+    <div>
+      <label htmlFor="opening-text">Opening_text</label>
+      <textarea rows='5' id="opening-text" ref={openingTextRef} />
+    </div>
+    <div>
+      <label htmlFor="date">Release Date</label>
+      <input type="text" id="date" ref={releaseDateRef} />
+    </div>
+    <button>Add Movie</button>
+  </form>
+}
+
+export default AddMovie;
